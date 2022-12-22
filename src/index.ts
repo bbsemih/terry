6a368d4842c1075476f7be00a7535523806f4fc8
Semih Berkay Öztürk <90466553+semihberkayozturk@users.noreply.github.com>
2022-12-22 13:14:29 +0300

    add methods inside for user class

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,6 @@
 import { User } from "./models/User";
 
-let user = new User({name:"semih",age:20})
+const user = new User({name:"new record",age:0});
 
-user.on("change",() => {
-    console.log("change event triggered!")
-});
-user.on("getElement",() => {
-    console.log("change getElement triggered!")
-});
 
-user.trigger("change");
-user.trigger("getElement");
+user.save()
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,7 @@
+import axios, { AxiosResponse } from "axios";
+
 interface UserProps {
+    id?:number,
     name?:string,
     age?:number
 };
@@ -30,4 +33,20 @@ export class User {
             callback();
         })
     };
+
+    fetch():void{
+        axios.get(`http://localhost:3000/users/${this.get(`id`)}`)
+        .then((res:AxiosResponse):void => {
+            this.set(res.data)
+        })
+    };
+
+    save():void{
+        const id = this.get("id");
+        if(id){
+            axios.put(`http://localhost:3000/users/${id}`,this.data);
+        }else{
+            axios.post(`http://localhost:3000/users/`,this.data);
+        }
+    }
 };
