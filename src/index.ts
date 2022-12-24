dccaab87636a2c9d475f5d793eaec59e1a4e1be7
Semih Berkay Öztürk <90466553+semihberkayozturk@users.noreply.github.com>
2022-12-24 22:14:12 +0300

    re-integrate eventing etc.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
-import { User } from "./models/User";
+import { User } from './models/User';
 
-const user = new User({name:"new record",age:0});
+const user = new User({name:"ahmetb",age:32});
 
+user.events.on("change",() => {
+    console.log("change");
+})
 
-user.save()
+user.events.trigger("change");
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,15 +1,15 @@
-import axios, { AxiosResponse } from "axios";
+import { Eventing } from './Eventing';
 
-interface UserProps {
+export interface UserProps {
     id?:number,
     name?:string,
     age?:number
 };
 
-type Callback = () => void;
-
 export class User {
-    events: {[key:string]:Callback[]} = {};
+    //Only accept properties into constructor.
+    //Hard code dependencies as class properties
+    public events:Eventing = new Eventing();   
 
     constructor(private data:UserProps){}
     get(propName:string):(number | string){
@@ -19,34 +19,4 @@ export class User {
     set(update:UserProps):void{
         Object.assign(this.data,update)
     };
-
-    on(eventName:string,callback:Callback):void{
-        const handlers = this.events[eventName] || [];
-        handlers.push(callback)
-        this.events[eventName] = handlers;
-    };
-
-    trigger(eventName:string):void{
-        const handlers = this.events[eventName];
-        if(!handlers || handlers.length === 0)return;
-        handlers.forEach(callback => {
-            callback();
-        })
-    };
-
-    fetch():void{
-        axios.get(`http://localhost:3000/users/${this.get(`id`)}`)
-        .then((res:AxiosResponse):void => {
-            this.set(res.data)
-        })
-    };
-
-    save():void{
-        const id = this.get("id");
-        if(id){
-            axios.put(`http://localhost:3000/users/${id}`,this.data);
-        }else{
-            axios.post(`http://localhost:3000/users/`,this.data);
-        }
-    }
 };
